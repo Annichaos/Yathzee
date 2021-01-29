01641daf77461e526646dd944e23dfe16c6e3708
unknown <annikabeissler@gmx.de>
2021-01-29 16:26:28 +0100

    module-info.java of the current development. The required java modules.

---
+++ Yathzee/src/module-info.java
@@ -0,0 +1,11 @@
+module Yathzee {
+
+	exports test.Yathzee;
+	
+	requires java.desktop;
+	requires java.management;
+	requires java.xml;
+	requires junit;
+	requires org.junit.jupiter.api;
+	requires javax.xml.bind;
+}
